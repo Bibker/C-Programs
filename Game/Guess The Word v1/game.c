71be96d9db42258395860f89404835ea12d76245
aashis-code <68364977+aashis-code@users.noreply.github.com>
2020-10-02 10:33:11 +0545

    Update game.c

--- Game/Guess The Word v1/game.c
+++ Game/Guess The Word v1/game.c
@@ -64,7 +64,7 @@ void p1playing(){
     printf("\n Your word has been stored sucessfully..\n");
     printf("Now Enter the same word with '_' in the middle\nEnter it ====>");
     scanf("%s",&halfWord);
-    printf("Press Enter..");
+    printf("Press Enter...");
     getch();
     system("cls");
     score();
